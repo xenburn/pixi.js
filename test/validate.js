cf0f72e3231ee657c4ba33409e34ded97161cb37
Matt Karl <matt.karl@jibo.com>
2016-05-05 11:31:19 -0400

    Fixed webgl test detection logic

--- test/validate.js
+++ test/validate.js
@@ -6,16 +6,15 @@ const assert = require('assert');
 
 before(function(){
 
-    this.hasWebGL = !!PIXI.utils.isWebGLSupported();
-    console.log("  PIXI.utils.isWebGLSupported:", this.hasWebGL);
-
-    if (!this.hasWebGL) {
+    // WebGL is not support on Travis, so let's check for it first
+    // WebGL tests will only get run locally
+    this.hasWebGL = PIXI.utils.isWebGLSupported();
+    if (this.hasWebGL) {
         const webglView = this.webglView = document.createElement('canvas');
         webglView.id = "webgl";
         webglView.width = 32;
         webglView.height = 32;
         document.body.appendChild(webglView);
-
         this.webgl = new PIXI.WebGLRenderer(32, 32, {
             view: webglView,
             backgroundColor: 0xffffff,
@@ -24,12 +23,12 @@ before(function(){
         });
     }
 
+    // Create the canvas renderer
     const canvasView = this.canvasView = document.createElement('canvas');
     canvasView.id = "canvas";
     canvasView.width = 32;
     canvasView.height = 32;
     document.body.appendChild(canvasView);
-
     this.canvas = new PIXI.CanvasRenderer(32, 32, {
         view: canvasView,
         backgroundColor: 0xffffff,
@@ -39,9 +38,12 @@ before(function(){
     });
     this.canvas.smoothProperty = null;
 
+    // Container to render
     this.stage = new PIXI.Container();
-    this.update = function() {
-        if (!this.hasWebGL) {
+
+    // Render function
+    this.render = function() {
+        if (this.hasWebGL) {
             this.webgl.render(this.stage);
         }
         this.canvas.render(this.stage);
@@ -52,18 +54,18 @@ before(function(){
     const solutions = require(__dirname + '/solutions.json'); 
 
     this.validate = function(testId) {
-        this.update();
-        if (!this.hasWebGL) {
+        this.render();
+        if (this.hasWebGL) {
             assert.equal(
                 solutions[testId].webgl, 
                 md5(this.webglView.toDataURL()), 
-                'WebGL solution for "' + testId + '" failed'
+                'WebGL solution for "' + testId + '" failed ' + this.webglView.toDataURL()
             );
         }
         assert.equal(
             solutions[testId].canvas, 
             md5(this.canvasView.toDataURL()), 
-            'Canvas solution for "' + testId + '" failed'
+            'Canvas solution for "' + testId + '" failed ' + this.canvasView.toDataURL()
         );
     };
 });
@@ -71,28 +73,25 @@ before(function(){
 // Delete and remove the canvas
 after(function(){
     this.stage.destroy(true);
-    if (!this.hasWebGL) {
+    if (this.hasWebGL) {
         this.webgl.destroy();
         this.webgl = null;
         this.webglView = null;
-        this.webglView.parentNode.removeChild(this.webglView);
     }
     this.canvas.destroy();
     this.canvas = null;
-    this.canvasView.parentNode.removeChild(this.canvasView);
     this.canvasView = null;
     this.stage = null;
     this.validate = null;
-    this.update = null;
+    this.render = null;
 });
 
 // Remove all children, cleanup
 beforeEach(function(){
     this.stage.removeChildren();
-    this.update();
 });
 
 afterEach(function(){
     this.stage.removeChildren();
-    this.update();
+    this.render();
 });
